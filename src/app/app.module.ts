3ce5ab0d1f3e77b93604bc5819e0598eaf112bb9
vanderlivrlj <vanderlidev@gmail.com>
2023-07-16 21:02:55 -0300

    Iniciando o projeto Pokémon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { PaginationComponent } from './componentes/pagination/pagination.component';
+import { ButtonComponent } from './componentes/button/button.component';
+import { HomeModule } from "./home/home.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    PaginationComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
